bb2164f0e76cb86abce8e209231737f5f1e494d3
Rodrigo Gomes <rodrigogsn@gmail.com>
2020-05-21 17:21:06 -0300

    Fix style to prevent iOS auto-zooming on inputs

--- src/styles/Global.js
+++ src/styles/Global.js
@@ -568,10 +568,29 @@ const GlobalStyle = createGlobalStyle`
 
   /********** Input Form **********/
   @media (max-width: 700px) {
+    input[type="color"],
+    input[type="date"],
+    input[type="datetime"],
+    input[type="datetime-local"],
+    input[type="email"],
+    input[type="month"],
+    input[type="number"],
+    input[type="password"],
+    input[type="search"],
+    input[type="tel"],
+    input[type="text"],
+    input[type="time"],
+    input[type="url"],
+    input[type="week"],
+    select:focus,
+    textarea {
+      font-size: 16px;
+    }
+
     main {
       flex: none;
     }
-    
+
     nav {
       flex-direction: column;
       border-bottom: 1px solid #26323850;
