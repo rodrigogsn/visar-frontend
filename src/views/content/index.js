416229f9415eba138777762f6f04ae71b034b560
Rodrigo Gomes <rodrigogsn@gmail.com>
2020-05-28 14:03:04 -0300

    Update content

--- src/views/content/index.js
+++ src/views/content/index.js
@@ -1,7 +1,7 @@
 //// HOME ////
 export const _home = {
   title: `Agende online o emplacamento do seu veículo.`,
-  paragraph: `My parents didn't know much science; in fact, they didn't know science at all. But they could recognize a science book when they saw it, and they spent a lot of time at bookstores, combing the remainder tables for science books to buy for me.`,
+  paragraph: `Faça um agendamento online com a Visar Emplaca, e garanta o emplacamento do seu veículo. Efetuamos o serviço a domicílio para a Baixada Santista, ou em nosso próprio estabelecimento. Confira!`,
 };
 
 //// LOGIN ////
