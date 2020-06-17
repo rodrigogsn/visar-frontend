5f5b9aec4a1b6cf67cd65430a69ad5d30e0e0937
Rodrigo Gomes <rodrigogsn@gmail.com>
2020-06-17 00:45:32 -0300

    Netlify deploy

--- src/views/Appointment/Regiao.js
+++ src/views/Appointment/Regiao.js
@@ -1,84 +1,84 @@
-import React, { useState, useContext, useEffect } from "react";
-import { useHistory } from "react-router-dom";
-import Header from "./../../components/Header";
-import Footer from "./../../components/Footer";
-import { Title, Paragraph } from "./../../components/Elements";
-import { _regiao } from "./../../views/content";
+// import React, { useState, useContext, useEffect } from "react";
+// import { useHistory } from "react-router-dom";
+// import Header from "./../../components/Header";
+// import Footer from "./../../components/Footer";
+// import { Title, Paragraph } from "./../../components/Elements";
+// import { _regiao } from "./../../views/content";
 
-import api from "./../../services/api";
-import MainContext from "./../../MainContext";
+// import api from "./../../services/api";
+// import MainContext from "./../../MainContext";
 
-const Regiao = () => {
-  let history = useHistory();
+// const Regiao = () => {
+//   let history = useHistory();
 
-  const { profile, setLocation } = useContext(MainContext);
+//   const { profile, setLocation } = useContext(MainContext);
 
-  const [locations, setLocations] = useState("");
+//   const [locations, setLocations] = useState("");
 
-  const handleLocations = async () => {
-    await api
-      .get("/locations")
-      .then((response) => {
-        const data = response.data.map((location) => (
-          <span
-            className="buttonWide-container"
-            onClick={() => {
-              handleClick(location);
-            }}
-            key={location.id}
-          >
-            <div className="buttonWide customHeight">
-              <h2>{location.name}</h2>
-            </div>
-          </span>
-        ));
+//   const handleLocations = async () => {
+//     await api
+//       .get("/locations")
+//       .then((response) => {
+//         const data = response.data.map((location) => (
+//           <span
+//             className="buttonWide-container"
+//             onClick={() => {
+//               handleClick(location);
+//             }}
+//             key={location.id}
+//           >
+//             <div className="buttonWide customHeight">
+//               <h2>{location.name}</h2>
+//             </div>
+//           </span>
+//         ));
 
-        setLocations(data);
-      })
-      .catch((error) => {
-        console.log(error.response);
-      });
-  };
+//         setLocations(data);
+//       })
+//       .catch((error) => {
+//         console.log(error.response);
+//       });
+//   };
 
-  const handleClick = (location) => {
-    setLocation(location);
-    history.push("/local");
-  };
+//   const handleClick = (location) => {
+//     setLocation(location);
+//     history.push("/local");
+//   };
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
+//   useEffect(() => {
+//     window.scrollTo(0, 0);
 
-    if (!profile) {
-      history.push("/");
-    }
+//     if (!profile) {
+//       history.push("/");
+//     }
 
-    handleLocations();
-  }, []);
+//     handleLocations();
+//   }, []);
 
-  return (
-    <>
-      <Header />
+//   return (
+//     <>
+//       <Header />
 
-      <main className="default">
-        <header>
-          <Title text={_regiao.title} />
-          <Paragraph text={_regiao.paragraph} />
-        </header>
+//       <main className="default">
+//         <header>
+//           <Title text={_regiao.title} />
+//           <Paragraph text={_regiao.paragraph} />
+//         </header>
 
-        <div className="buttonGroup">
-          {locations}
-          <span className="buttonWide-container">
-            <div className="buttonWide customHeight disabled">
-              <h2>Outras Regiões</h2>
-              <p>(Em breve)</p>
-            </div>
-          </span>
-        </div>
-      </main>
+//         <div className="buttonGroup">
+//           {locations}
+//           <span className="buttonWide-container">
+//             <div className="buttonWide customHeight disabled">
+//               <h2>Outras Regiões</h2>
+//               <p>(Em breve)</p>
+//             </div>
+//           </span>
+//         </div>
+//       </main>
 
-      <Footer />
-    </>
-  );
-};
+//       <Footer />
+//     </>
+//   );
+// };
 
-export default Regiao;
+// export default Regiao;
